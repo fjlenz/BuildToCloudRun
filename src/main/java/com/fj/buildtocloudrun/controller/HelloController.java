d722083b30efb6580604fba24e21655ed2b1bbb0
FJ <37023430+fjlenz@users.noreply.github.com>
2026-01-31 20:06:38 +0100

    Update HelloController.java

--- src/main/java/com/fj/buildtocloudrun/controller/HelloController.java
+++ src/main/java/com/fj/buildtocloudrun/controller/HelloController.java
@@ -5,12 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 import java.util.Map;
 
 @RestController
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class HelloController {
 
     @GetMapping("/hello")
