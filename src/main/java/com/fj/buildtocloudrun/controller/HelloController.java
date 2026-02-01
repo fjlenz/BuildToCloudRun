51325d63453bca6c06b7475cc0405fc8bee56cb0
fjlenz <fjlenz@gmail.com>
2026-02-01 09:19:05 +0100

    set openapi server name

--- src/main/java/com/fj/buildtocloudrun/controller/HelloController.java
+++ src/main/java/com/fj/buildtocloudrun/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.fj.buildtocloudrun.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +11,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.Map;
 
 @RestController
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "This is the Server URL")})
 public class HelloController {
 
     @GetMapping("/hello")
-    @Operation(summary = "Simple hello endpoint")
+    @Operation(summary = "Simple hello endpoint taking name as input")
     public ResponseEntity<Map<String, String>> getHello(
             @RequestParam(value = "name") String name) {
         return ResponseEntity.ok(Map.of("message", "Hello " + name));
